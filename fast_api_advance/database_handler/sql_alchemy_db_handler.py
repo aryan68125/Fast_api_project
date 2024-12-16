5b1dbff8d2fb24696637936433c5dfa5ecf437d7
aryan68125 <aryan68125@gmail.com>
2024-12-16 09:55:14 +0530

    removed the print statement from database handler

--- fast_api_advance/database_handler/sql_alchemy_db_handler.py
+++ fast_api_advance/database_handler/sql_alchemy_db_handler.py
@@ -6,7 +6,6 @@ from decouple import config
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://<username>:<password>@<ip-address/hostname>/<database_name>"
 connection_string = f"postgresql://{config('DB_USERNAME')}:{config('DB_PASSWORD')}@{config('DB_IP')}/{config('DB_NAME')}"
-print(connection_string)
 db_engine  = create_engine(connection_string)
 
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=db_engine)
