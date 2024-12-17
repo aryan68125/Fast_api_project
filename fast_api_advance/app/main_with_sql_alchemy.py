f2d401e634f0145a70fc4c7c097fc799e3f7e97b
aryan68125 <aryan68125@gmail.com>
2024-12-17 15:56:57 +0530

    Routers in FastAPI implemented , seperated the api end-points of posts and users

---
+++ fast_api_advance/app/main_with_sql_alchemy.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, status, Depends
+
+
+#import sql alchemy model
+from . import sql_alchemy_models
+#import sql alchemy database engine
+from database_handler.sql_alchemy_db_handler import db_engine, SessionLocal, db_flush
+#import session from sql alchemy
+from sqlalchemy.orm import Session
+
+
+#import routers
+from .routers import posts, users
+
+app = FastAPI()
+
+#routes that handles all the posts
+app.include_router(posts.router)
+#routes that handles all the users
+app.include_router(users.router)
+
+sql_alchemy_models.Base.metadata.create_all(bind=db_engine)
--- fast_api_advance/app/routers/users.py
+++ fast_api_advance/app/routers/users.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status, Depends
+from fastapi import FastAPI, status, Depends, APIRouter
 
 #utilities
 from utility.common_response import response
@@ -12,7 +12,7 @@ from utility.common_error_messages import (
 )
 
 #import sql alchemy model
-from . import sql_alchemy_models
+from .. import sql_alchemy_models
 #import sql alchemy database engine
 from database_handler.sql_alchemy_db_handler import db_engine, SessionLocal, db_flush
 #import session from sql alchemy
@@ -24,7 +24,6 @@ from sqlalchemy import desc
 #make url parameters optional
 from typing import Optional
 #usae pydantic model to define the structure of the data that is to be inserted in the api end-point
-from pydantic_custom_models.Posts import InsertPostsModel, UpdatePostsModel, SoftDeleteRestorePostsModel, RatingPostsModel
 from pydantic_custom_models.Users import CreateUpdateUserModel, BlockUnblockUsersModel, SoftDeleteRestoreUserModel, VerifyOTPUsersModel, ResendOtp, RequestResetPasswordModel, ResetPasswordModel
 
 #import a utility that hashes user password
@@ -35,97 +34,12 @@ from utility.send_mail import send_email_async, send_email_background
 from fastapi import BackgroundTasks
 from random import randint
 
-app = FastAPI()
+router = APIRouter()
 
 sql_alchemy_models.Base.metadata.create_all(bind=db_engine)
 
-#Create a post
-@app.post('/post')
-def create_post(post : InsertPostsModel,db : Session = Depends(db_flush)):
-    new_post = sql_alchemy_models.posts_sql_alchemy_table(
-        **post.model_dump()
-    )
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-    if not new_post:
-        return response(status=status.HTTP_400_BAD_REQUEST,error=DATA_INSERT_ERR)
-    return response(status=status.HTTP_201_CREATED,message=DATA_INSERT_SUCCESS,data=new_post)
-
-#get all rows from the table using sql alchemy
-@app.get('/posts',)
-def get_all_posts(db:Session=Depends(db_flush)):
-    # This is gonna grab every single entry withing the posts_sql_alchemy_table
-    # posts = db.query(sql_alchemy_models.posts_sql_alchemy_table).all()
-    posts = db.query(sql_alchemy_models.posts_sql_alchemy_table).order_by(desc(sql_alchemy_models.posts_sql_alchemy_table.id)).all()
-    if not len(posts):
-        return response(status=status.HTTP_404_NOT_FOUND,error=DATA_NOT_FOUND_ERR)
-    return response(status=status.HTTP_200_OK,message=DATA_SENT_SUCCESS,data=posts)
-
-#get one row from the table using sql alchemy
-@app.get('/posts/{id}',)
-def get_one_post(id:int, db: Session = Depends(db_flush)):
-    post = db.query(sql_alchemy_models.posts_sql_alchemy_table).filter(sql_alchemy_models.posts_sql_alchemy_table.id == id).first()
-    if not post:
-        return response(status=status.HTTP_404_NOT_FOUND,error=DATA_NOT_FOUND_ERR)
-    return response(status=status.HTTP_200_OK,message=DATA_SENT_SUCCESS,data=post)
-
-#Update post
-@app.patch('/posts/{id}')
-def update_post(id:int,postModel : UpdatePostsModel ,db: Session = Depends(db_flush)):
-    post_query = db.query(sql_alchemy_models.posts_sql_alchemy_table).filter(sql_alchemy_models.posts_sql_alchemy_table.id == id)
-    post = post_query.first()
-    if not post:
-         return response(status = status.HTTP_404_NOT_FOUND,error=DATA_NOT_FOUND_ERR)
-    post_query.update(postModel.model_dump(), synchronize_session=False)
-    db.commit()
-    return response(status=status.HTTP_200_OK,message=DATA_UPDATE_SUCCESS,data=post_query.first())
-
-# Update rating of a post
-@app.patch('/post/rate-posts/{id}')
-def rate_post(id:int, PostModel : RatingPostsModel,db:Session = Depends(db_flush)):
-    post_query = db.query(sql_alchemy_models.posts_sql_alchemy_table).filter(sql_alchemy_models.posts_sql_alchemy_table.id == id)
-    post = post_query.first()
-    if not post:
-         return response(status=status.HTTP_404_NOT_FOUND,error=DATA_NOT_FOUND_ERR)
-    post_query.update(PostModel.model_dump(),synchronize_session=False)
-    db.commit()
-    return response(status=status.HTTP_200_OK,message=DATA_UPDATE_SUCCESS,data=post_query.first())
-
-#Soft delete or restore posts
-@app.patch('/posts/soft-delete-or-restore/{id}')
-def soft_delete_or_restore(id:int, PostModel : SoftDeleteRestorePostsModel, db : Session = Depends(db_flush)):
-    post_query = db.query(sql_alchemy_models.posts_sql_alchemy_table).filter(sql_alchemy_models.posts_sql_alchemy_table.id == id)
-    post = post_query.first()
-    if not post:
-         return response(status = status.HTTP_404_NOT_FOUND,error=DATA_NOT_FOUND_ERR)
-    PostModelUpdated = PostModel.model_dump()
-    PostModelUpdated['is_published'] = not PostModelUpdated.get('is_deleted')
-    post_query.update(PostModelUpdated, synchronize_session=False)
-    db.commit()
-    if PostModel.is_deleted:
-         return response(status=status.HTTP_200_OK,message=DATA_SOFT_DELETE_SUCCESS,data=post_query.first())
-    return response(status=status.HTTP_200_OK,message=DATA_RESTORE_SUCCESS,data=post_query.first())
-
-#Hard delete post
-@app.delete('/post/{id}')
-def hard_delete_post(id:int, db: Session = Depends(db_flush)):
-    existing_post = db.query(sql_alchemy_models.posts_sql_alchemy_table).filter(
-        sql_alchemy_models.posts_sql_alchemy_table.id == id
-    )
-    post_exists = existing_post.first()
-    if not post_exists:
-        return response(status=status.HTTP_404_NOT_FOUND,error=DATA_NOT_FOUND_ERR)
-    existing_post.delete(synchronize_session=False)
-    db.commit()
-    return response(status=status.HTTP_200_OK,message=DATA_HARD_DELETE_SUCCESS)       
-
-
-
-
-
 #Create a user in a database table
-@app.post('/users/register')
+@router.post('/users/register')
 def create_users(userModel : CreateUpdateUserModel, background_tasks : BackgroundTasks, db : Session = Depends(db_flush)):
     try:
        # before we create the user we need to create the hash of the password
@@ -171,7 +85,7 @@ def create_users(userModel : CreateUpdateUserModel, background_tasks : Backgroun
         return response(status=status.HTTP_500_INTERNAL_SERVER_ERROR,error=e)
 
 #verify otp that's sent to the user
-@app.patch('/users/verify-otp')
+@router.patch('/users/verify-otp')
 def verify_otp(otpModel : VerifyOTPUsersModel, db : Session = Depends(db_flush)):
     try:
         otp_dict = otpModel.model_dump()
@@ -204,7 +118,7 @@ def verify_otp(otpModel : VerifyOTPUsersModel, db : Session = Depends(db_flush))
         return response(status=status.HTTP_500_INTERNAL_SERVER_ERROR, error=str(e))
 
 #Resend otp to the user if requested by them
-@app.patch('/users/send-mail/resend-otp/')
+@router.patch('/users/send-mail/resend-otp/')
 def resend_otp(resend_otp_model : ResendOtp, background_tasks : BackgroundTasks,db : Session = Depends(db_flush)):
     resend_otp_dict = resend_otp_model.model_dump()
     user_pk = resend_otp_dict.get('id')
@@ -247,7 +161,7 @@ def resend_otp(resend_otp_model : ResendOtp, background_tasks : BackgroundTasks,
     return response(status=status.HTTP_200_OK,message=MAIL_SENT_SUCCESS,data=response_data)
 
 # Request a password change for the users whose account is activated
-@app.patch('/users/send-mail/request-reset-password')
+@router.patch('/users/send-mail/request-reset-password')
 def request_reset_password(request_reset_password_model : RequestResetPasswordModel, background_tasks : BackgroundTasks, db : Session = Depends(db_flush)):
     request_reset_password_dictionary = request_reset_password_model.model_dump()
     email = request_reset_password_dictionary.get('email')
@@ -278,7 +192,7 @@ def request_reset_password(request_reset_password_model : RequestResetPasswordMo
     return response(status = status.HTTP_200_OK,message=MAIL_SENT_SUCCESS,data=response_data)
 
 # Verify the otp generated for reset password
-@app.patch('/users/verify-otp/reset-password')
+@router.patch('/users/verify-otp/reset-password')
 def verify_otp_reset_password(reset_password_model : VerifyOTPUsersModel, db : Session = Depends(db_flush)):
     reset_password_dict = reset_password_model.model_dump()
     id = reset_password_dict.get('id')
@@ -294,7 +208,7 @@ def verify_otp_reset_password(reset_password_model : VerifyOTPUsersModel, db : S
     return response(status=status.HTTP_200_OK,message=OTP_VERIFICATION_SUCCESS)
 
 # After otp verification allow user to reset his account's password
-@app.patch('/users/reset-password')
+@router.patch('/users/reset-password')
 def reset_password(reset_password_model : ResetPasswordModel, db:Session = Depends(db_flush)):
     reset_password_model_dict = reset_password_model.model_dump()
     id = reset_password_model_dict.get('id')
@@ -313,7 +227,7 @@ def reset_password(reset_password_model : ResetPasswordModel, db:Session = Depen
 
 # Create an api end-point that must return the logged in user's detail 
 # If you are not able to access this api then your access token is expired and you need to get a new access token.
-@app.get('/users/{id}')
+@router.get('/users/{id}')
 def get_user_details(id : int, db : Session = Depends(db_flush)):
     user = db.query(sql_alchemy_models.UserMaster).filter(sql_alchemy_models.UserMaster.id == id, sql_alchemy_models.UserMaster.is_deleted == False).first()
     if not user:
@@ -329,4 +243,4 @@ def get_user_details(id : int, db : Session = Depends(db_flush)):
 
 # Login user
 
-# Logout user
+# Logout user     
